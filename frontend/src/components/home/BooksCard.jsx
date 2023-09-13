3968854008ac3ff617e67ced9700d8290e769c71
MdKaioumislam5 <afrankaiyum34@gmail.com>
2023-09-13 19:09:47 +0600

    updated forntend-site BooksCard

--- frontend/src/components/home/BooksCard.jsx
+++ frontend/src/components/home/BooksCard.jsx
@@ -1,17 +1,5 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-// import React from "react";
-// import BookSingleCard from "./BookSingleCard";
-
-// const BooksCard = ({ books }) => {
-//   return (
-//     <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-//       {books.map((item) => (
-//         <BookSingleCard key={item._id} book={item}></BookSingleCard>
-//       ))}
-//     </div>
-//   );
-// };
 
 // export default BooksCard;
 import React from "react";
